e45239befbdcbec3a1f4a257a4c5e5c1b6591011
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-08-21 11:29:10 -0300

    Criação da Modal da tela Cartões

---
+++ app/javascript/components/CardsDialog.js
@@ -0,0 +1,88 @@
+import React, {useEffect, useState} from "react"
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function CardsDialog({currentCard, modalOpen, handleCloseModal}) {
+
+  const [last4Digits, setLast4Digits] = useState('');
+  const [cardUser, setCardUser] = useState('');
+  const [isNew, setIsNew] = useState(false);
+
+  useEffect(() => {
+    setLast4Digits(currentCard?.last4 || '');
+    setCardUser(currentCard?.user || '');
+    setIsNew(currentCard === null || currentCard?.id === undefined);
+  }, [currentCard]);
+
+  function handleSubmit(event) {
+    // TODO: Implement API submission
+    event.preventDefault();
+    console.log(last4Digits);
+    console.log(cardUser);
+    handleCloseModal();
+  }
+
+  return (
+    <Dialog
+      fullWidth
+      maxWidth="sm"
+      keepMounted
+      open={modalOpen}
+      PaperProps={{
+        component: 'form',
+        onSubmit: handleSubmit
+      }}
+      slotProps={{backdrop: {sx: {background: 'rgba(0, 0, 0, 0.12)'}}}}
+    >
+      <DialogTitle sx={{p: 2}}>{isNew ? "Cadastar Cartão" : "Associar Funcionário"}</DialogTitle>
+
+      <IconButton onClick={handleCloseModal}
+                  sx={{
+                    position: 'absolute',
+                    right: 8,
+                    top: 8
+                  }}>
+        <CloseIcon/>
+      </IconButton>
+      <DialogContent sx={{p: 2}}>
+        {isNew && <React.Fragment>
+          <DialogContentText>
+            Informe os dados
+          </DialogContentText>
+          <TextField
+            required
+            margin="dense"
+            id="last4"
+            label="Número"
+            type="text"
+            fullWidth
+            variant="standard"
+            value={last4Digits}
+            onChange={(event) => setLast4Digits(event.target.value)}
+          />
+        </React.Fragment>}
+        <TextField
+          required
+          margin="dense"
+          id="user"
+          label={`${isNew ? "Funcionário" : "E-mail"}`}
+          type="text"
+          fullWidth
+          variant="standard"
+          value={cardUser}
+          onChange={(event) => setCardUser(event.target.value)}
+        />
+      </DialogContent>
+      <DialogActions sx={{justifyContent: 'flex-start', p: 2}}>
+        <Button variant='contained' type="submit">{currentCard?.id ? "Salvar" : "Cadastar"}</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- app/javascript/components/CardsPage.js
+++ app/javascript/components/CardsPage.js
@@ -1,20 +1,67 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React, {useState} from "react"
 import Layout from "./Layout";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+import CreditCardIcon from "@mui/icons-material/CreditCardOutlined";
+import AddIcon from "@mui/icons-material/Add";
+import CardsDialog from "./CardsDialog";
 
-const CardsPage = ({ menu, cards }) => {
+const CardsPage = ({menu, cards}) => {
+
+
+  const [modalOpen, setmodalOpen] = useState(false);
+  const [currentCard, setCurrentCard] = useState(null);
+
+  const handleOpenModal = (_event, cardId) => {
+    setCurrentCard(cards.find(card => card.id === cardId));
+    setmodalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setmodalOpen(false);
+  };
+
+  function CardsList({cards}) {
+    return (
+      <List>
+        {cards.map((card) => (
+          <React.Fragment key={card.id}>
+            <ListItem secondaryAction={<Button variant="outlined" onClick={(e) => handleOpenModal(e, card.id)}
+                                               endIcon={<EditIcon/>}>Editar</Button>}>
+              <ListItemAvatar>
+                <Avatar>
+                  <CreditCardIcon/>
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={`Cartão ${card.user?.name}`} secondary={`**** **** **** ${card.last4}`}/>
+            </ListItem>
+            <Divider/>
+          </React.Fragment>
+        ))}
+      </List>
+    );
+  }
+
   return (
     <Layout currentMenu={menu}>
       <Container maxWidth="lg">
         <Box component="main">
-          <Typography py={2} variant='h4'>Cartões</Typography>
-          <ul>
-            {cards.map((card) => <li key={card.id}>{card.last4}</li>)}
-          </ul>
+          <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
+            <Typography py={2} variant='h4'>Cartões</Typography>
+            <Button onClick={handleOpenModal} variant="contained" endIcon={<AddIcon/>}>Cadastrar Cartão</Button>
+          </Box>
+          <CardsList cards={cards}/>
         </Box>
+        <CardsDialog currentCard={currentCard} modalOpen={modalOpen} handleCloseModal={handleCloseModal}/>
       </Container>
     </Layout>
   )
