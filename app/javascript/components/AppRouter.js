7c94cc7880cf65f0eadf17de1ccf4629d61ca08e
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-08-23 17:11:46 -0300

    Adicionado informações de usuário ao AppBar

--- app/javascript/components/AppRouter.js
+++ app/javascript/components/AppRouter.js
@@ -3,16 +3,21 @@ import StatementsPage from "./StatementsPage";
 import EmployeesPage from "./EmployeesPage";
 import CardsPage from "./CardsPage";
 import CategoriesPage from "./CategoriesPage";
+import {createContext, useState} from "react";
+
+export const UserContext = createContext();
 
 export default function AppRouter(props) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/despesas" element={<StatementsPage menu={props.page} statements={props.statements}/>}/>
-        <Route path="/funcionarios" element={<EmployeesPage menu={props.page} employees={props.employees}/>}/>
-        <Route path="/cartoes" element={<CardsPage menu={props.page} cards={props.cards}/>}/>
-        <Route path="/categorias" element={<CategoriesPage menu={props.page} categories={props.categories}/>}/>
-      </Routes>
-    </BrowserRouter>
+    <UserContext.Provider value={props.currentUser}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/despesas" element={<StatementsPage menu={props.page} statements={props.statements}/>}/>
+          <Route path="/funcionarios" element={<EmployeesPage menu={props.page} employees={props.employees}/>}/>
+          <Route path="/cartoes" element={<CardsPage menu={props.page} cards={props.cards}/>}/>
+          <Route path="/categorias" element={<CategoriesPage menu={props.page} categories={props.categories}/>}/>
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   )
 }
--- app/javascript/components/Layout.js
+++ app/javascript/components/Layout.js
@@ -8,10 +8,22 @@ import Sidebar from "./Sidebar";
 import {Outlet} from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import LogoutIcon from '@mui/icons-material/Logout';
+import {useContext} from "react";
+import {UserContext} from "./AppRouter";
+import Avatar from "@mui/material/Avatar";
 
 const drawerWidth = 240;
 
 export default function Layout(props) {
+
+  const currentUser = useContext(UserContext)
+
+  function NameInitials() {
+    const [firstName, lastName] = currentUser?.name?.split(' ') || [''];
+
+    return lastName ? `${firstName[0]}${lastName[0]}` : firstName[0];
+  }
+
   return (
     <Box sx={{display: 'flex'}}>
       <CssBaseline/>
@@ -20,8 +32,13 @@ export default function Layout(props) {
           <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
             Espresso
           </Typography>
-          <IconButton color="inherit" href='/users/sign_out' rel='nofollow' data-method='delete' >
-            <LogoutIcon />
+          <Avatar>{NameInitials()}</Avatar>
+          <Box sx={{mx: 2}}>
+            <Typography variant="body2">{currentUser.name}</Typography>
+            <Typography variant="caption">{currentUser.email}</Typography>
+          </Box>
+          <IconButton size="medium" color="inherit" href='/users/sign_out' rel='nofollow' data-method='delete' >
+            <LogoutIcon fontSize="large" />
           </IconButton>
         </Toolbar>
       </AppBar>
