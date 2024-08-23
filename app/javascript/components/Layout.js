4ad5438a288b926f415b7844f07ba7d376a8ee80
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-08-23 15:40:36 -0300

    Adicionado botão Logout ao AppBar

--- app/javascript/components/Layout.js
+++ app/javascript/components/Layout.js
@@ -6,24 +6,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Sidebar from "./Sidebar";
 import {Outlet} from "react-router-dom";
+import IconButton from "@mui/material/IconButton";
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const drawerWidth = 240;
 
 export default function Layout(props) {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+    <Box sx={{display: 'flex'}}>
+      <CssBaseline/>
+      <AppBar position="fixed" sx={{zIndex: (theme) => theme.zIndex.drawer + 1}}>
         <Toolbar>
-          <Typography variant="h6" noWrap component="div">
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
             Espresso
           </Typography>
+          <IconButton color="inherit" href='/users/sign_out' rel='nofollow' data-method='delete' >
+            <LogoutIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
-      <Sidebar currentMenu={props.currentMenu} drawerWidth={drawerWidth} />
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <Toolbar />
-        {props.children || <Outlet />}
+      <Sidebar currentMenu={props.currentMenu} drawerWidth={drawerWidth}/>
+      <Box component="main" sx={{flexGrow: 1, p: 3}}>
+        <Toolbar/>
+        {props.children || <Outlet/>}
       </Box>
     </Box>
   );
