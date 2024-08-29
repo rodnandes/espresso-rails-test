6c60c45f695dff81e16ac90efe96d0070e099f79
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-08-29 18:45:33 -0300

    Correção dos testes para envio de senha via email

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -2,16 +2,26 @@ require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
   describe "send_password" do
-    let(:mail) { UserMailer.send_password }
+    let(:password) { "password123" }
+    let(:user) { build(:user) }
+    let(:mail) { UserMailer.send_password(user, password) }
+    let(:email_body_contents) { mail.body.parts.map {|part| part.body.encoded }.join }
+    let(:text_content) { mail.body.parts.find { |p| p.content_type.match 'text/plain' }.body.raw_source }
+    let(:html_content) { mail.body.parts.find { |p| p.content_type.match 'text/html' }.body.raw_source }
 
-    it "renders the headers" do
+
+    it "has the correct headers" do
       expect(mail.subject).to eq("Send password")
-      expect(mail.to).to eq(["to@example.org"])
+      expect(mail.to).to eq([user.email])
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it "has the correct text content" do
+      expect(text_content).to include("Olá, sua senha de acesso é: #{password}")
+    end
+
+    it "has the correct html content" do
+      expect(html_content).to include("Olá, sua senha é: #{password}")
     end
   end
 
