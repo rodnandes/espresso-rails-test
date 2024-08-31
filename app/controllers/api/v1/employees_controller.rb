dd9b1f4327564b8647ed59cdb6b641c81b792d44
Rodrigo Fernandes <1895242+rodnandes@users.noreply.github.com>
2024-08-31 01:41:25 -0300

    Criação dos testes para endpoint /api/v1/employees

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,48 +1,54 @@
-class Api::V1::EmployeesController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+# frozen_string_literal: true
 
-  # GET /api/v1/employees.json
-  def index
-    @employees = current_user.company.employees
-  end
+module Api
+  module V1
+    class EmployeesController < ApplicationController
+      before_action :set_user, only: %i[show update destroy]
 
-  # GET /api/v1/employees/1.json
-  def show
-  end
+      # GET /api/v1/employees.json
+      def index
+        @employees = current_user.company.employees
+      end
 
-  # POST /api/v1/employees.json
-  def create
-    @employee = User.new(user_params.merge(company: current_user.company))
+      # GET /api/v1/employees/1.json
+      def show; end
 
-    if @employee.save
-      render :show, status: :created
-    else
-      render json: @employee.errors, status: :unprocessable_entity
+      # POST /api/v1/employees.json
+      def create
+        @employee = User.new(user_params.merge(company: current_user.company))
+
+        if @employee.save
+          render :show, status: :created
+        else
+          render json: @employee.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /api/v1/employees/1.json
+      def update
+        if @employee.update(user_params)
+          render :show, status: :ok
+        else
+          render json: @employee.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /api/v1/employees/1.json
+      def destroy
+        @employee.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_user
+        @employee = User.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def user_params
+        params.fetch(:user).permit(:name, :email)
+      end
     end
   end
-
-  # PATCH/PUT /api/v1/employees/1.json
-  def update
-    if @employee.update(user_params)
-      render :show, status: :ok
-    else
-      render json: @employee.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /api/v1/employees/1.json
-  def destroy
-    @employee.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @employee = User.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.fetch(:user).permit(:name, :email)
-    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :authorized?
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, company_attributes: [:name, :cnpj]])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, { company_attributes: %i[name cnpj] }])
   end
 
+  def authorized?
+    return false if current_user.admin?
+
+    render json: { error: 'You are not allowed to perform this action.' }, status: :forbidden
+  end
+
+  def not_found
+    render json: { error: 'Record not found.' }, status: :not_found
+  end
 end
